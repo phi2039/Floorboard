a80bc02d76991c30ac1e77e5e71bdbe577398ba2
Chris Lance <climber2039@gmail.com>
2014-04-02 17:09:46 -0400

    EEPROM Handling

---
+++ Arduino/93L46B.h
@@ -0,0 +1,147 @@
+
+#define READ  0x02 // 10 00ABCD - 0x02 ADDR
+#define EWEN1 0x00 // 00 11XXXX
+#define EWEN2 0x30 //           - 0x00 0x30
+#define ERASE 0x03 // 11 00ABCD - 0x03 ADDR
+#define ERAL1 0x00 // 00 10XXXX
+#define ERAL2 0x20 //           - 0x00 0x20
+#define WRITE 0x01 // 01 00ABCD - 0x01 ADDR
+#define WRAL1 0x00 // 00 01XXXX
+#define WRAL2 0x10 //           - 0x00 0x10
+#define EWDS1 0x00 // 00 00XXXX
+#define EWDS2 0x00 //           - 0x00 0x00
+
+
+#define DO 3
+#define DI 2
+#define PIN93C46  PINB
+#define PORT93C46 PORTB
+
+typedef void (*SelectSlavePtr)(unsigned int index);
+class SPI_93C46B
+{
+public:
+  SPI_93C46B(unsigned int slaveIndex, SelectSlavePtr ptr) :
+    SelectSlave(ptr),
+    m_SlaveIndex(slaveIndex)
+  {      
+  }
+
+  uint16_t Read(uint8_t address) 
+  {
+    uint16_t data = 0;
+
+    Opcode(READ, address);
+
+    // "0" Dummy Bit
+    digitalWrite(11, LOW);
+    digitalWrite(13, HIGH);
+    digitalWrite(13, LOW);
+
+    SPCR |=  (1 << CPHA);               // Invert CPHA, very important!
+
+    data = SPI.transfer(0);
+    data = (data << 8) | (SPI.transfer(0));
+    delayMicroseconds(1);
+    SelectSlave(0xFF);
+    digitalWrite(11, LOW);
+    SPCR &= ~(1 << CPHA);    
+
+    return data;
+  }
+  
+  void EraseWriteEnable(void) 
+  {
+    Opcode(EWEN1, EWEN2);
+    SelectSlave(0xFF);
+    digitalWrite(11, LOW);
+    delayMicroseconds(0.1);
+    SelectSlave(m_SlaveIndex);
+    while(!(PIN93C46 & (1 << DO))); // Wait 1
+    SelectSlave(0xFF);
+  }
+  
+  void EraseWriteDisable(void) 
+  {
+    Opcode(EWDS1, EWDS2);
+    SelectSlave(0xFF);
+    digitalWrite(11, LOW);
+    delayMicroseconds(0.1);
+    SelectSlave(m_SlaveIndex);
+    while(!(PIN93C46 & (1 << DO))); // Wait 1
+    SelectSlave(0xFF);
+  }
+  
+  void Erase(uint8_t address) 
+  {
+    Opcode(ERASE, address);   
+    digitalWrite(11, LOW);
+    SelectSlave(0xFF);
+    SelectSlave(m_SlaveIndex);
+    while(!(PIN93C46 & (1 << DO))); // Wait 1
+    delayMicroseconds(0.1);
+    SelectSlave(0xFF);
+  }
+  
+  void EraseAll(void) 
+  {
+    Opcode(ERAL1, ERAL2);   
+    digitalWrite(11, LOW);
+    SelectSlave(0xFF);
+    SelectSlave(m_SlaveIndex);
+    while(!(PIN93C46 & (1 << DO))); // Wait 1
+    delayMicroseconds(0.1);
+    SelectSlave(0xFF);
+  }
+  
+  void Write(uint8_t address, uint16_t data) 
+  {
+    Opcode(WRITE, address);
+    SPI.transfer(data >> 8);            // Write data high byte to address
+    SPI.transfer(data & 0xFF);          // Write data low byte to address
+
+    digitalWrite(11, LOW);
+    SelectSlave(0xFF);
+    SelectSlave(m_SlaveIndex);
+    delayMicroseconds(0.1);
+    while(!(PIN93C46 & (1 << DO))); // Wait "1"
+    SelectSlave(0xFF);
+  }
+  
+  void WriteAll(uint16_t data) 
+  {
+    Opcode(WRAL1, WRAL2);
+    SPI.transfer(data >> 8);            // Write data high byte to address
+    SPI.transfer(data & 0xFF);          // Write data low byte to address
+
+    digitalWrite(11, LOW);
+    SelectSlave(0xFF);
+    SelectSlave(m_SlaveIndex);
+    delayMicroseconds(0.1);
+    while(!(PIN93C46 & (1 << DO))); // Wait "1"
+    SelectSlave(0xFF);
+  }
+
+private:
+  void Opcode(uint8_t opcode, uint8_t address) 
+  {
+    SelectSlave(m_SlaveIndex);
+
+    // Take manual control to send the start bit
+    SPCR &= ~(1 << SPE);               // SPI disable
+    delayMicroseconds(1);
+
+    // Send the start bit
+    digitalWrite(13, LOW);
+    digitalWrite(11, HIGH);
+    digitalWrite(13, HIGH);
+    digitalWrite(13, LOW);
+
+    SPCR |= (1 << SPE);                // SPI enable
+
+    SPI.transfer((opcode << 6) | address); // Transmit byte
+  }
+
+  SelectSlavePtr SelectSlave;
+  unsigned int m_SlaveIndex;
+};
--- Arduino/Arduino.ino
+++ Arduino/Arduino.ino
@@ -1,41 +1,54 @@
 
 #include <SPI.h>
+#include "93L46B.h"
 
 // Address Bus Definitions
+// There are two registers (LOW/HIGH)
+// Digital I/O Modules are arranged in "banks" attached to the LOW register (addresses 0-7)
+// Each bank consists of one input module and one output module (creating a single, virtual I/O device)
+// Other peripherals are attached to the HIGH register (addresses 8-15)
+// All SPI devices share the same serial bus (MOSI, MISO, SCK)
 #define A0_PIN       2
-#define A1_PIN       4
-#define A2_PIN       7
-#define ADDR_EN_PIN  8 // Active low
+#define A1_PIN       3
+#define A2_PIN       4
+#define A3_PIN       5 // Bank Select 
+#define ADDR_EN_PIN  9 // Active LOW
 
 // I/O Control Definitions
 #define SLOAD_PIN   10  // Serial Load (Inputs only)
 
 // External I/O Module Configuration
-#define IO_BANK_COUNT          4
-#define BANK_NONE         0xFF
+#define IO_BANK_COUNT     4
+#define SLAVE_NONE         0xFF
 
 // I/O Scanning
-#define MODULE_IO_WIDTH  8 // Number of bits handled by each I/O module (so we can chain shift registers later, if we want to, for a wider IO range)
+#define MODULE_IO_WIDTH  8 // Number of bits handled by each I/O module (so we can chain shift registers later, if we want to, for a wider I/O range)
 
 #define RISING_EDGE  0
 #define FALLING_EDGE 1
 
+// Forward Declarations
+unsigned int ScanDigitalIOBank(int bankIndex, unsigned int outputVal);
+void SelectSPISlave(unsigned int index);
+
 // Global Variables
 unsigned int g_DigitalInputStates[IO_BANK_COUNT];
 unsigned int g_DigitalOutputStates[IO_BANK_COUNT];
+unsigned int g_ActiveSPISlave;
 
-// Forward Declarations
-unsigned int ScanDigitalIOBank(int bankIndex, unsigned int outputVal);
+SPI_93C46B g_EEPROM(8, SelectSPISlave);
 
-//#define DEBUG_MON // Enables serial port monitoring of input/output state changes
+#define DEBUG_MON // Enables serial port monitoring of input/output state changes
 
 // Utility Functions ///////////////////////
 #ifdef DEBUG_MON
   #define PrintInputStates(b) _PrintInputStates(b)
   #define PrintOutputStates(b) _PrintOutputStates(b)
+  #define PrintBuffer(a,b) _PrintBuffer(a,b)
 #else
   #define PrintInputStates(b)
   #define PrintOutputStates(b)
+  #define PrintBuffer(a,b)
 #endif
 
 void _PrintInputStates(int bank)
@@ -77,9 +90,40 @@ void _PrintOutputStates(int bank)
   
   Serial.print("\r\n");  
 }
+
+void _PrintBuffer(uint8_t* pBuf, unsigned int len)
+{
+  Serial.print("[0x");
+  Serial.print((unsigned int)pBuf, HEX);
+  Serial.print("]: ");
+
+  for (int i = 0; i < len; i++)
+  {
+    Serial.print(pBuf[i], HEX);
+    Serial.print(" ");
+  }
+  Serial.print("\r\n");  
+}
+
+void _PrintBuffer(uint16_t* pBuf, unsigned int len)
+{
+  Serial.print("[0x");
+  Serial.print((unsigned int)pBuf, HEX);
+  Serial.print("]: ");
+
+  for (int i = 0; i < len; i++)
+  {
+    Serial.print(pBuf[i], HEX);
+    Serial.print(" ");
+  }
+  Serial.print("\r\n");  
+}
 ///////////////////////////////////////////
 
+// DUMMY VARIABLES FOR TESTING ///////////////////////////////////////////
 byte latch[IO_BANK_COUNT];
+uint16_t eeprom_data[4];
+//////////////////////////////////////////////////////////////////////////
 
 void setup() 
 {
@@ -94,6 +138,7 @@ void setup()
   pinMode(A0_PIN, OUTPUT);
   pinMode(A1_PIN, OUTPUT);
   pinMode(A2_PIN, OUTPUT);    
+  pinMode(A3_PIN, OUTPUT);    
   pinMode(ADDR_EN_PIN, OUTPUT);
   
   // I/O Control
@@ -121,37 +166,119 @@ void setup()
   }
   
 // Set Default Hardware State
+  g_ActiveSPISlave = 0xFF;
   memset(g_DigitalOutputStates, 0, sizeof(g_DigitalOutputStates)); // Initialize all external outputs LOW
   for (int bank = 0; bank < IO_BANK_COUNT; bank++)
     ScanDigitalIOBank(bank, g_DigitalOutputStates[bank]);
   memset(g_DigitalInputStates, 0, sizeof(g_DigitalInputStates)); // Initialize all external inputs LOW
 
-
+// TEMP TESTING STUFF TO CLEAN UP LATER
   memset(latch, 0, sizeof(latch));
+  memset(eeprom_data, 0, sizeof(eeprom_data));
+ }
+
+// Persistent Storage Handling ///////////////////////////////////////
+void WriteEEPROM(uint8_t address, uint16_t val)
+{
+  g_EEPROM.EraseWriteEnable();
+  g_EEPROM.Write(address, val);
+  g_EEPROM.EraseWriteDisable();
 }
 
-// I/O Module Handling ///////////////////////////////////////
-void SelectDigitalIOBank(int moduleId)
+void WriteEEPROM(uint8_t address, uint8_t len, uint16_t* pBuf)
 {
+  g_EEPROM.EraseWriteEnable();
+ // TODO: Do Something
+  g_EEPROM.EraseWriteDisable();
+}
+
+void EraseEEPROM()
+{
+  g_EEPROM.EraseWriteEnable();
+  g_EEPROM.EraseAll();
+  g_EEPROM.EraseWriteDisable();
+}
+
+void EraseEEPROM(uint8_t address)
+{
+  g_EEPROM.EraseWriteEnable();
+  g_EEPROM.Erase(address);
+  g_EEPROM.EraseWriteDisable();
+}
+
+uint16_t ReadEEPROM(uint8_t address)
+{
+  return g_EEPROM.Read(address);
+}
+
+uint16_t* ReadEEPROM(uint8_t address, uint8_t len)
+{
+ // TODO: Do Something
+  return NULL;
+}
+
+// Test Functions
+void FillEEPROM()
+{
+  g_EEPROM.EraseWriteEnable();
+  for (uint8_t i = 0; i < 64; i++)
+    g_EEPROM.Write(i, i); // TODO: How about a more interesting/useful pattern...
+
+  g_EEPROM.EraseWriteDisable();
+}
+
+void DumpEEPROM()
+{
+  uint16_t data[4];
+  for (int i = 0; i < 64; i+=4)
+  {
+    for (int a = 0; a < 4; a++)
+      data[a] = g_EEPROM.Read(i+a);
+    
+    PrintBuffer(data, 4);
+  }
+}
+
+//////////////////////////////////////////////////////////////////////
+
+// SPI Bus Selection ///////////////////////////////////////
+void SelectSPISlave(unsigned int address)
+{
+  // See if this slave is already active (don't change it if it's already active)
+  if (address != g_ActiveSPISlave)
+  {
     // Disable demux output
     digitalWrite(ADDR_EN_PIN, HIGH);
-    if (moduleId != BANK_NONE) // If BANK_IDX_NONE is specified, disable all modules
+    if (address != SLAVE_NONE) // If BANK_IDX_NONE is specified, disable all modules
     {
       // Write address bits
-      // TODO: Is there a function to write all these at once?
-      digitalWrite(A0_PIN, (moduleId & 0x1) ? HIGH : LOW);
-      digitalWrite(A1_PIN, (moduleId & 0x2) ? HIGH : LOW);
-      digitalWrite(A2_PIN, (moduleId & 0x4) ? HIGH : LOW);
+      // TODO: Is there a function to write all these at once? Ports...
+      digitalWrite(A0_PIN, (address & 0x1) ? HIGH : LOW);
+      digitalWrite(A1_PIN, (address & 0x2) ? HIGH : LOW);
+      digitalWrite(A2_PIN, (address & 0x4) ? HIGH : LOW);
+      digitalWrite(A3_PIN, (address & 0x8) ? HIGH : LOW);
       
-      // Enable demux output
-      digitalWrite(ADDR_EN_PIN, LOW);  
+      // Enable demux output(s)
+      digitalWrite(ADDR_EN_PIN, LOW);
     }
+    g_ActiveSPISlave = address; // Store the address
+  }  
+}
+//////////////////////////////////////////////////////////////////////////////
+
+// I/O Module Handling ///////////////////////////////////////
+void SelectDigitalIOBank(unsigned int bank)
+{
+  SelectSPISlave(bank & 0x7); // Mask out register bit (A3) (TODO: Does this mask make any sense...?)
 }
 
 // Scans one bank of digital I/O (one input module, one output module). Writes outputVal to selected output module and returns input value from selected input module
 unsigned int ScanDigitalIOBank(int bankIndex, unsigned int outputVal)
 {
-  // Pulse the LOAD pin (inputs) to store input states in shift registers
+  // Make sure the SPI flags are set how we want them for these devices
+   SPI.setDataMode(SPI_MODE0); // POL = 0 (rising edge), CPHA = 0 (first edge)
+
+ // Pulse the LOAD pin (inputs) to store input states in shift registers
   digitalWrite(SLOAD_PIN, LOW); // Copy input states into shift register
   delayMicroseconds(5); // let the values settle in the shift register (TODO: Do we really need this? The datasheet says it only takes 15ns to load...)
   digitalWrite(SLOAD_PIN, HIGH); // Reset latch pin (data has been captured)
@@ -163,7 +290,7 @@ unsigned int ScanDigitalIOBank(int bankIndex, unsigned int outputVal)
   unsigned int inputVal = SPI.transfer(~outputVal);
   
   // Reset BUS_ENABLE (stop shifting inputs) and STCP (latch outputs)
-  SelectDigitalIOBank(BANK_NONE);
+  SelectDigitalIOBank(SLAVE_NONE);
   
  return inputVal;
 }
@@ -208,7 +335,7 @@ void loop()
           if ((oldVal >> i) & 0x1)
             OnDigitalInputChange(bank, i, FALLING_EDGE);
           else
-            OnDigitalInputChange(bank, i, RISING_EDGE);       
+            OnDigitalInputChange(bank, i, RISING_EDGE);  
         }
         inputFlags >>= 1; // Shift right one bit
       }
